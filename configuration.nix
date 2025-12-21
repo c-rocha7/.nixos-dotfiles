51e2679576953af01621329360e4c551e17dbd0b
Cauã R. Pereira <7aauac@gmail.com>
2025-12-21 14:23:37 -0300

    update config

--- configuration.nix
+++ configuration.nix
@@ -40,9 +40,14 @@
     ];
   };
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
+  hardware = {
+    enableAllFirmware = true;
+    firmware = [ pkgs.linux-firmware ];
+
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
   };
 
   networking.hostName = "nixos";
@@ -162,10 +167,8 @@
   programs.obs-studio = {
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
       obs-pipewire-audio-capture
-      obs-vaapi
-      obs-gstreamer
+      obs-vkcapture
     ];
   };
 
