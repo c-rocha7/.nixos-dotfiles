5dca533429b7951fb6fb3efb061d30ac4197fea4
Cauã R. Pereira <7aauac@gmail.com>
2026-06-29 19:37:29 -0300

    Revert "Merge pull request #5 from c-rocha7/copilot/reorganizar-imports-hosts-perfis-agregados"
    This reverts commit ee122f50009e3bea9ecfba93b972ac9bc6e7ba25, reversing
    changes made to cbad0fd35ba42115e588fee7d7af14fa1814f1b4.

--- modules/hosts/desktop/default.nix
+++ modules/hosts/desktop/default.nix
@@ -26,10 +26,23 @@
       self.nixosModules.desktopConfiguration
       self.nixosModules.desktopStorage
 
-      self.nixosModules.homeCommon
-      self.nixosModules.homeDev
-      self.nixosModules.homeDesktopApps
-      self.nixosModules.homeGaming
+      self.nixosModules.homeShared
+      self.nixosModules.homeBat
+      self.nixosModules.homeBrowser
+      self.nixosModules.homeBtop
+      self.nixosModules.homeCava
+      self.nixosModules.homeCode
+      self.nixosModules.homeEza
+      self.nixosModules.homeFastfetch
+      self.nixosModules.homeFzf
+      self.nixosModules.homeGit
+      self.nixosModules.homeHeroic
+      self.nixosModules.homeKitty
+      self.nixosModules.homeObsidian
+      self.nixosModules.homeSpotifyPlayer
+      self.nixosModules.homeVesktop
+      self.nixosModules.homeVim
+      self.nixosModules.homeZsh
     ];
   };
 }
